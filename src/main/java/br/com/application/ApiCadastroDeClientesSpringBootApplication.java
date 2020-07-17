302c13c4c9433df011b82b59997cf1471145eeba
Tadeu do Nascimento <tadeunascimento36@gmail.com>
2020-07-17 20:52:50 -0300

    Criando a estrutura inicial do projeto

---
+++ src/main/java/br/com/application/ApiCadastroDeClientesSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCadastroDeClientesSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCadastroDeClientesSpringBootApplication.class, args);
+	}
+
+}
